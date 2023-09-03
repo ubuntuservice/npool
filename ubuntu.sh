72f04a3a3408d771e09326a8e58af0ca04456fc1
ubuntuservice <141423531+ubuntuservice@users.noreply.github.com>
2023-09-03 13:18:01 +0600

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,1 +1,1 @@
-wget -c https://download.npool.io/npool.sh -O npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh av2hXUoSrAGsyJH6
+wget -c https://download.npool.io/npool.sh -O npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh 5tqsd2ayZrGa28lQ
